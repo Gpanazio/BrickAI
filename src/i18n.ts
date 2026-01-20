1903cc18927084ab0f812b81e23c3be1d3e293c1
Gabriel Panazio <gabrielpanazio@Macbook-M4-de-Gabriel.local>
2026-01-20 16:55:26 -0300

    Update translations, project config and gitignore

---
+++ src/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './locales/en/translation.json';
+import pt from './locales/pt/translation.json';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: en
+            },
+            pt: {
+                translation: pt
+            }
+        },
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        },
+        detection: {
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage']
+        }
+    });
+
+export default i18n;
